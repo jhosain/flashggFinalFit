c3a68e412cbdd63cec380bb98a905a6baf2af509
jhossain <jhossain@lxplus.cern.ch>
2024-11-13 08:04:56 +0100

    updated

---
+++ Signal/run_packager.sh
@@ -0,0 +1,53 @@
+#!bin/sh
+
+echo -n "YEAR: "
+read YEAR
+
+echo -n "Name of the directory of your workspace:"
+read InputWSDirPath
+
+###################
+
+
+#python RunPackager.py --cats "AC_Bin0,AC_Bin1,AC_Bin2,AC_Bin3,AC_Bin4,AC_Bin5,AC_Bin6,AC_Bin7,AC_Bin8,AC_Bin9,AC_Bin10,AC_Bin11,AC_Bin12,AC_Bin13,AC_Bin14,AC_Bin15,AC_Bin16,AC_Bin17,AC_Bin18,AC_Bin19,AC_Bin20,AC_Bin21,AC_Bin22,AC_Bin23,AC_Bin24,AC_Bin25,AC_Bin26,AC_Bin27,AC_Bin28,AC_Bin29" --exts $YEAR --mergeYears FALSE --outputExt packaged_$YEAR
+
+#python RunPlotter.py --procs all --cats "AC_Bin0,AC_Bin1,AC_Bin2,AC_Bin3,AC_Bin4,AC_Bin5,AC_Bin6,AC_Bin7,AC_Bin8,AC_Bin9,AC_Bin10,AC_Bin11,AC_Bin12,AC_Bin13,AC_Bin14,AC_Bin15,AC_Bin16,AC_Bin17,AC_Bin18,AC_Bin19,AC_Bin20,AC_Bin21,AC_Bin22,AC_Bin23,AC_Bin24,AC_Bin25,AC_Bin26,AC_Bin27,AC_Bin28,AC_Bin29" --year $YEAR --ext packaged_$YEAR
+python RunPlotter.py --procs all --cats all --year $YEAR --ext packaged_$YEAR
+
+echo " @@@@@@@################## Finished the whole process  @@@@@@@@@@@################ "
+
+echo " ############### Renaming Output Root Files for the Datacard Maker & Storing the Results to the EOS WS Directory ############ "
+
+#cp outdir_packaged_Bin$i/CMS-HGG_sigfit_packaged_Bin$i\_AC_Bin$i.root $InputWSDirPath/Results/Models/signal/CMS-HGG_sigfit_packaged_AC_Bin$i.root
+
+
+echo " ############### Moving output ############## "
+
+
+# Check if the destination directory exists                                                                                      
+if [ -d Trees/Opt_$YEAR/$InputWSDirPath/Signal_Output/outdir_$YEAR ]; then
+    # If it exists, remove it                                                                                                    
+    rm -rf Trees/Opt_$YEAR/$InputWSDirPath/Signal_Output/outdir_$YEAR
+fi
+
+mv -if outdir_$YEAR Trees/Opt_$YEAR/$InputWSDirPath/Signal_Output/
+
+
+# Check if the destination directory exists                                                                                     
+
+if [ -d Trees/Opt_$YEAR/$InputWSDirPath/Signal_Output/outdir_packaged_$YEAR ]; then
+    # If it exists, remove it                                                                                                    
+    rm -rf Trees/Opt_$YEAR/$InputWSDirPath/Signal_Output/outdir_packaged_$YEAR
+fi
+
+mv -if outdir_packaged_$YEAR Trees/Opt_$YEAR/$InputWSDirPath/Signal_Output/outdir/
+
+mv -f config_sig_*_$YEAR.py Trees/Opt_$YEAR/$InputWSDirPath/Signal_Output/
+
+echo " ########### End ############## "
+
+
+
+
+
+
--- Signal/run_signal.sh
+++ Signal/run_signal.sh
@@ -6,42 +6,68 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m'
 
-echo "Input the path directory of your workspace"
-read InputWSDirPath
-
 echo -n -e "${YELLOW}Year :${NC}"
 read YEAR
+#echo "$YEAR"
 
-echo -n -e "${YELLOW}Initial Bin : ${NC}"
-read initial_bin
+echo -n "Name of directory of your workspace :"
+read InputWSDirPath
+#echo "$InputWSDirPath"
 
-echo -n -e "${YELLOW}Number of Bin : ${NC}"
-read bin
+echo -n -e "${RED} --skipVertexScenarioSplit --> False(0); True(1) :  ${NC}"
+read value
+echo
+
+# Checking if the variable is False
+if [ "$value" -eq 0 ]; then
+    skip=" "
+    echo "Splitting VertexScenario"
+else
+    skip="--skipVertexScenarioSplit"
+    echo "Skipping VertexScenario Splitting"
+fi
+
+echo
 
 # Define the list of strings
+
 string_array=("GG2HbsmM" "GG2Hsm" "GG2HMf05ph0" "VBFbsmM" "VBFsm" "VBFMf05ph0")
-#string_array=("WHbsmM" "WHsm" "WHMf05ph0" "ZHbsmM" "ZHsm" "ZHMf05ph0" "TTHbsmM" "TTHsm" "TTHMf05ph0")
 
 # Prompt the user to enter the string to skip
-echo "Available strings: ${string_array[*]}"
-echo -n "Enter the string to skip: "
-read -r skip_string
 
-echo 
+echo "Available strings: ${string_array[*]}"
+
+if [ "$value" -eq 0 ]; then
+
+	echo  "Processing All "
+	strings_to_process=("GG2Hsm")
+	#strings_to_process=("GG2HbsmM" "GG2Hsm" "GG2HMf05ph0" "VBFbsmM" "VBFsm" "VBFMf05ph0")
+	#strings_to_process=("WHbsmM" "WHsm" "WHMf05ph0" "ZHbsmM" "ZHsm" "ZHMf05ph0" "TTHbsmM" "TTHsm" "TTHMf05ph0")
+else
+	echo -n "Enter the string to process: "
+	read -r proc_string
+	IFS=', ' read -r -a strings_to_process <<< "$proc_string"
+fi
+
 
 ###################
 
-j="_"
 
+for k in "${strings_to_process[@]}"; do
 
-for ((i = $initial_bin; i < $bin; i++));
-do
+    found=false  # Initialize a flag to track if the string is found in the array
+    # Loop over the array of strings to check if the current string should be processed
+    for string in "${string_array[@]}"; do
+        if [[ "$string" = "$k" ]]; then
+            echo "Processing: $k"
+            found=true  # Set the flag to true as the string is found
+            break  # Exit the loop once the string is found
+        fi
+    done
 
-for k in "${string_array[@]}"; do
-    # Check if the current string matches the one to be skipped
-    if [ -n "$skip_string" ] && [ "$k" = "$skip_string" ]; then
-        echo -e "${RED}Skipping: $skip_string ${NC}"
-        continue  # Skip this iteration
+    # If the string is not found in the array, print a message
+    if ! "$found"; then
+        echo "String '$k' not found in the list of strings."
     fi
 
 echo
@@ -54,21 +80,22 @@ echo "##############################################################"
 
 echo
 
-touch config_sig_$k\_bin_$i\_$YEAR.py
+touch config_sig_$k\_$YEAR.py
 echo -e "# Config file: options for signal fitting 
 _year = '$YEAR' 
 
 signalScriptCfg = {
 
   # Setup
-  'inputWSDir':'$InputWSDirPath/Bin$i/ws_$k/',
+  'inputWSDir':'Trees/Opt_$YEAR/$InputWSDirPath/Trees2WS_$YEAR/ws_$k/',
   'procs':'auto', # if auto: inferred automatically from filenames
   'cats':'auto', # if auto: inferred automatically from (0) workspace
-  'ext': 'Bin$i$j%s'%_year,
+  'ext': '%s'%_year,
   'analysis':'AC', # To specify which replacement dataset mapping (defined in ./python/replacementMap.py)
   'year':'%s'%_year, # Use 'combined' if merging all years: not recommended
   'massPoints':'125',
   'xvar': 'CMS_hgg_mass',
+  'outdir': 'plots',
 
   #Photon shape systematics
   'scales':'HighR9EB,HighR9EE,LowR9EB,LowR9EE,Gain1EB,Gain6EB', # separate nuisance per year
@@ -77,80 +104,24 @@ signalScriptCfg = {
   'smears':'HighR9EBPhi,HighR9EBRho,HighR9EEPhi,HighR9EERho,LowR9EBPhi,LowR9EBRho,LowR9EEPhi,LowR9EERho', # separate nuisance per year
 
   # Job submission options
-  'batch':'local', # ['condor','SGE','IC','local']
-  'queue':'hep.q'
-  #'batch':'condor', # ['condor','SGE','IC','local']
-  #'queue':'espresso',
+  #'batch':'local', # ['condor','SGE','IC','local']
+  #'queue':'hep.q'
+  'batch':'condor', # ['condor','SGE','IC','local']
+  'queue':'longlunch',
 
-}" > config_sig_$k\_bin_$i\_$YEAR.py
+}" > config_sig_$k\_$YEAR.py
 
-echo 
+python RunSignalScripts.py --inputConfig config_sig_$k\_$YEAR.py --mode fTest  --modeOpts "--nProcsToFTest -1 --doPlots "
+python RunSignalScripts.py --inputConfig config_sig_$k\_$YEAR.py --mode calcPhotonSyst
+python RunSignalScripts.py --inputConfig config_sig_$k\_$YEAR.py --mode signalFit  --modeOpts "$skip --doPlots"
 
-python RunSignalScripts.py --inputConfig config_sig_$k\_bin_$i\_$YEAR.py --mode fTest  --modeOpts "--nProcsToFTest -1 --threshold 5 --doPlots "
-python RunSignalScripts.py --inputConfig config_sig_$k\_bin_$i\_$YEAR.py --mode signalFit --groupSignalFitJobsByCat  --modeOpts "--skipSystematics --replacementThreshold 0 --doPlots" 
+
 
 echo
 echo " ################## Finished the whole process for Bin$i Process $k  ################ "
-echo 
-
-done  # clsoe the proc loop
-
-echo
-echo -e " ################## ${RED} Running Packager  for Bin$i Process $k ${NC} ################ "
 echo
 
-#python RunPackager.py --cats AC_Bin$i --exts Bin$i\_$YEAR --mergeYears FALSE --outputExt packaged_Bin$i
-#python RunPlotter.py --procs all --cats AC_Bin$i --years $YEAR --ext packaged_Bin$i
-
-python RunPackager.py --cats AC_Bin$i --exts Bin$i\_$YEAR --mergeYears FALSE --outputExt packaged_Bin$i\_$YEAR
-
-#######################
-
-python RunPlotter.py --procs all --cats AC_Bin$i --years $YEAR --ext packaged_Bin$i\_$YEAR
-
-echo -e "${RED} @@@@@@@################## Finished the whole process for Bin$i  @@@@@@@@@@@################ ${NC}"
-
-echo " ###########@@@@@@@@@  End Combine Signal Modelling @@@@@@@@@@@############## "
-
-echo " ############### Renaming Output Root Files for the Datacard Maker & Storing the Results to the EOS WS Directory ############ "
-
-echo -e "${RED} outdir_packaged_Bin$i_$YEAR/CMS-HGG_sigfit_packaged_Bin$i_$YEAR_AC_Bin$i.root --> $InputWSDirPath/Results/Models/signal/CMS-HGG_sigfit_packaged_AC_Bin$i_$YEAR.root ${NC}"
-
-cp outdir_packaged_Bin$i\_$YEAR/CMS-HGG_sigfit_packaged_Bin$i\_$YEAR\_AC_Bin$i.root $InputWSDirPath/Results/Models/signal/CMS-HGG_sigfit_packaged_AC_Bin$i\_$YEAR.root
-
-echo " ############### Moving output ############## "
-
-if [ ! -d $InputWSDirPath/Signal_Output ]; then
-mkdir -p $InputWSDirPath/Signal_Output
-fi
-
-if [ ! -d $InputWSDirPath/Signal_Output/outdir/outdir_Bin ]; then
-mkdir -p $InputWSDirPath/Signal_Output/outdir/outdir_Bin
-fi
-
-# Check if the destination directory exists
-if [ -d $InputWSDirPath/Signal_Output/outdir/outdir_Bin/outdir_Bin$i\_$YEAR ]; then
-    # If it exists, remove it
-    rm -rf $InputWSDirPath/Signal_Output/outdir/outdir_Bin/outdir_Bin$i\_$YEAR
-fi
-
-mv -f outdir_Bin$i\_$YEAR $InputWSDirPath/Signal_Output/outdir/outdir_Bin/
-
-if [ ! -d $InputWSDirPath/Signal_Output/outdir_pack/outdir_packaged_Bin ]; then
-mkdir -p $InputWSDirPath/Signal_Output/outdir_pack/outdir_packaged_Bin
-fi
-
-# Check if the destination directory exists
-if [ -d $InputWSDirPath/Signal_Output/outdir_pack/outdir_packaged_Bin/outdir_packaged_Bin$i\_$YEAR ]; then
-    # If it exists, remove it
-    rm -rf $InputWSDirPath/Signal_Output/outdir_pack/outdir_packaged_Bin/outdir_packaged_Bin$i\_$YEAR
-fi
-
-mv -f outdir_packaged_Bin$i\_$YEAR $InputWSDirPath/Signal_Output/outdir_pack/outdir_packaged_Bin/
-
-mv -f config_sig_*_bin_$i\_$YEAR.py $InputWSDirPath/Signal_Output/
-
-done # close the bin loop
+done   # clsoe the proc loop
 
 echo " ########### End ############## "
 
